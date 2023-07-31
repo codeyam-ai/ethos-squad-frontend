f2ac7787d4c553f8259b22e88b63bbc681698059
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-07-31 13:30:56 -0700

    add custom sign in modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,24 @@ import Home from "./components/Home";
 function App() {
   return (<EthosConnectProvider
     ethosConfiguration={{
-      chain: Chain.SUI_TESTNET // Optional. Defaults to sui:devnet - An enum containing acceptable chain identifier strings can be imported from the ethos-connect package 
+      chain: Chain.SUI_TESTNET
     }}
+    dappName='Ethos Squad'
+    dappIcon={<SquadDappIcon />}
+    connectMessage="Sign in to join the Squad!"
   >
     <Home />
   </EthosConnectProvider>);
 }
 
 export default App;
+
+const SquadDappIcon = () => {
+  return (
+    <img
+      src="/mimi-card-stack.png"
+      alt=""
+      className="w-48 h-48"
+    />
+  )
+}
