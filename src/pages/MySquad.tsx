5896364a8a203ab33942407281c32545e9b75bb0
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-07-31 14:55:36 -0700

    add wallet not connected, loading, and empty state to my squad page

--- src/pages/MySquad.tsx
+++ src/pages/MySquad.tsx
@@ -1,9 +1,14 @@
-import { ethos } from "ethos-connect";
+import { EthosConnectStatus, ethos } from "ethos-connect";
 import { BackgroundProvider } from "../components/BackgroundProvider";
 import { TESTNET_NFT_CONTRACT } from "../contracts";
+import { useMemo } from "react";
 
 const MySquad: React.FC = () => {
-    const { wallet } = ethos.useWallet();
+    const { status, wallet } = ethos.useWallet();
+
+    const squadNfts = useMemo(() => {
+        return wallet?.contents?.nfts?.filter((nft) => nft.packageObjectId === TESTNET_NFT_CONTRACT)
+    }, [wallet?.contents?.nfts])
 
     return (
         <BackgroundProvider>
@@ -25,25 +30,28 @@ const MySquad: React.FC = () => {
                     <div className="w-full mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                         <ul role="list" className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                             {
-                                wallet?.contents?.nfts?.filter((nft) => nft.packageObjectId === TESTNET_NFT_CONTRACT).map((nft) => {
-                                    console.log('nft :>> ', nft);
-                                    return (
-                                        <li key={nft.objectId} className="relative">
-                                            <div className="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                                                <img
-                                                    className="pointer-events-none object-cover group-hover:opacity-75"
-                                                    src={nft.imageUrl}
-                                                    alt={nft.name}
-                                                />
-                                                <button type="button" className="absolute inset-0 focus:outline-none">
-                                                    <span className="sr-only">View details for {nft.name}</span>
-                                                </button>
-                                            </div>
-                                            <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{nft.name}</p>
-                                            <p className="pointer-events-none block text-sm font-medium text-gray-500">{nft.description}</p>
-                                        </li>
-                                    )
-                                })
+                                status !== EthosConnectStatus.Connected ? <p className="text-gray-500">Connect your wallet to see your Squad NFTs!</p> :
+                                    !squadNfts ? <p className="text-gray-500">Loading...</p> :
+                                        squadNfts.length === 0 ? <p className="text-gray-500 text-center w-full">You don't have any Squad NFTs yet!</p> :
+                                            squadNfts.map((nft) => {
+                                                console.log('nft :>> ', nft);
+                                                return (
+                                                    <li key={nft.objectId} className="relative">
+                                                        <div className="group aspect-w-10 aspect-h-7 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                                                            <img
+                                                                className="pointer-events-none object-cover group-hover:opacity-75"
+                                                                src={nft.imageUrl}
+                                                                alt={nft.name}
+                                                            />
+                                                            <button type="button" className="absolute inset-0 focus:outline-none">
+                                                                <span className="sr-only">View details for {nft.name}</span>
+                                                            </button>
+                                                        </div>
+                                                        <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{nft.name}</p>
+                                                        <p className="pointer-events-none block text-sm font-medium text-gray-500">{nft.description}</p>
+                                                    </li>
+                                                )
+                                            })
                             }
                         </ul>
                     </div>
