e9d5c7b4123196bee076c7f3351bebc1c483bc84
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-07-31 14:37:25 -0700

    extract out BackgroundProvider

--- src/pages/MySquad.tsx
+++ src/pages/MySquad.tsx
@@ -1,73 +1,32 @@
 import { ethos } from "ethos-connect";
+import { BackgroundProvider } from "../components/BackgroundProvider";
 
 const MySquad: React.FC = () => {
     const { status, wallet } = ethos.useWallet();
 
     return (
-        <div className="bg-white">
-            <main>
-                <div className="relative isolate">
-                    <svg
-                        className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
-                        aria-hidden="true"
-                    >
-                        <defs>
-                            <pattern
-                                id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
-                                width={200}
-                                height={200}
-                                x="50%"
-                                y={-1}
-                                patternUnits="userSpaceOnUse"
-                            >
-                                <path d="M.5 200V.5H200" fill="none" />
-                            </pattern>
-                        </defs>
-                        <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-                            <path
-                                d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-                                strokeWidth={0}
-                            />
-                        </svg>
-                        <rect width="100%" height="100%" strokeWidth={0} fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
-                    </svg>
-                    <div
-                        className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
-                        aria-hidden="true"
-                    >
-                        <div
-                            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-                            style={{
-                                clipPath:
-                                    'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
-                            }}
-                        />
+        <BackgroundProvider>
+            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                    <div className="lg:shrink-0 xl:max-w-2xl">
+                        <span className='flex gap-2'>
+                            <h1 className="text-4xl font-light tracking-tight text-gray-900 sm:text-6xl">
+                                Your
+                            </h1>
+                            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                                Squad
+                            </h1>
+                        </span>
+                        <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                            This is your squad. Bask in its glory.
+                        </p>
                     </div>
-                    <div className="overflow-hidden">
-                        <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
-                            <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
-                                <div className="lg:shrink-0 xl:max-w-2xl">
-                                    <span className='flex gap-2'>
-                                        <h1 className="text-4xl font-light tracking-tight text-gray-900 sm:text-6xl">
-                                            Your
-                                        </h1>
-                                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                            Squad
-                                        </h1>
-                                    </span>
-                                    <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                                        This is your squad. Bask in its glory.
-                                    </p>
-                                </div>
-                                <div className="w-full mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-                                    hi
-                                </div>
-                            </div>
-                        </div>
+                    <div className="w-full mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                        hi
                     </div>
                 </div>
-            </main>
-        </div>
+            </div>
+        </BackgroundProvider>
     );
 }
 
