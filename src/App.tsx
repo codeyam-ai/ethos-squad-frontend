e68142af9e2428d3f2da627f5c491f3219d437c1
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-07-31 13:05:31 -0700

    add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Home from "./components/Home";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>);
+    <div>
+      <Home />
+    </div>);
 }
 
 export default App;
